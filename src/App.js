5cb14026b7f3d3f77f4332fa19d17371b6995561
aneesaali <aneesaali@users.noreply.github.com>
2020-10-25 16:24:33 -0400

    completed initial sign up page Need to: - route Submit button to send information to database - add validation for proper email address - add validation for email address existing in database - add validation for empty fields - add "terms and conditions" above checkbox

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <NewProfile />
     </div>
   );
--- src/components/NewProfile.js
+++ src/components/NewProfile.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from "react";
 import { Component } from "react";
+import './styles.css'
 
 // Color Palette
 // Dark Pink: #FF3576
@@ -28,51 +29,62 @@ class NewProfile extends Component {
         this.handleChange = this.handleChange.bind(this)
       }
 
-      handleChange(event) {
+    handleChange(event) {
         const {name, value, type, checked} = event.target
-        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({[name]: value})
+        type === "checkbox" ? this.setState({[name]: checked}) : this.setState({[name]: value})
     }
 
     handleSubmit(event) {
-        const {password, retype} = this.state
-        if (retype.value !== password.value){
-            alert("no match")
-           return false;
+        if (event.target.password.value !== event.target.retype.value){
+            event.preventDefault()
+            alert("Your passwords do not match.")
+       }
+       //else if (if form fields are empty){
+           //event.preventDefault()
+           //put alert
+       //}
+       else {
+        //insert code to send information to database      
        }
     }
 
     render(){
         return (
-            //<header class>Welcome to Oink! To begin, please fill in the following information:</header>
-            //<h1>Welcome to Oink!</h1>
             <form onSubmit={this.handleSubmit}>
+                <h1>Welcome to Oink!</h1>
                 <h2>Your Information</h2>
-                First Name
+                <label>First Name
                 <input type="text" value={this.state.firstName} name="firstName" onChange={this.handleChange} />
                 <br/>
-                Last Name 
+                </label>
+                <label>Last Name 
                 <input type="text" value={this.state.lastName} name="lastName" onChange={this.handleChange} />
                 <br/>
-                Email Address 
+                </label>
+                <label>Email Address 
                 <input type="text" value={this.state.email} name="email" onChange={this.handleChange} />
                 <br/>
+                </label>
                 <h2>Account Information</h2>
-                Username 
+                <label>Username 
                 <input type="text" value={this.state.username} name="username"onChange={this.handleChange} />
                 <br/>
-                Password 
+                </label>
+                <label>Password 
                 <input type="text" value={this.state.password} name="password" onChange={this.handleChange} />
                 <br/>
-                Retype Password 
+                </label>
+                <label>Retype Password
                 <input type="text" value={this.state.retype} name="retype" onChange={this.handleChange} />
-                <h1>{this.state.accept.value}</h1>
                 <br/>
+                <br/>
+                </label>
                <label>
                     <input type="checkbox" checked={this.state.accept} name="accept" onChange={this.handleChange} />
-               I accept the terms and conditions.</label> 
+               I accept the terms and conditions.
+               </label>
                <br/>
-               <br/>
-               <button>Submit</button>
+               <button disabled = {this.state.accept !== true} >Sign Up</button>
             </form>
             )
     }
