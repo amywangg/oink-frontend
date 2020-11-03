e1b115e669a81de6f4de6ec780e02575439eabec
Amy <amy_wangg@hotmail.com>
2020-11-02 22:02:56 -0500

    Revert "Revert "Add redux state management for authentication""
    This reverts commit b3445903763afd213363bb637525f0bb8389b3a3.

--- src/containers/AppPage/index.js
+++ src/containers/AppPage/index.js
@@ -1,9 +1,5 @@
-import React, { memo, useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-
-import { compose } from "redux";
-import { withRouter } from "react-router-dom";
-import { createStructuredSelector } from "reselect";
 
 import clsx from "clsx";
 import Drawer from "@material-ui/core/Drawer";
@@ -15,32 +11,16 @@ import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import { createMuiTheme } from "@material-ui/core/styles";
-import { ThemeProvider } from "@material-ui/styles";
-import pink from "@material-ui/core/colors/pink";
+
 import HomeIcon from "@material-ui/icons/Home";
 import SettingsIcon from "@material-ui/icons/Settings";
 
 import usePersistedState from "../PersistedState";
 import { useStyles } from "./styles";
+import Logout from "../Auth/Logout";
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: pink[50],
-    },
-    secondary: {
-      main: pink[100],
-    },
-  },
-});
-
-const AppPage = withRouter(({ children, history }) => {
+const AppPage = ({ children }) => {
   const classes = useStyles();
-
-  const handleRoute = (route) => {
-    history.push(route);
-  };
 
   const [open, setOpen] = usePersistedState("drawer", false);
 
@@ -54,81 +34,74 @@ const AppPage = withRouter(({ children, history }) => {
 
   return (
     <div className={classes.root}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+      <CssBaseline />
 
-        <Drawer
-          variant="permanent"
-          className={clsx(classes.drawer, {
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open,
-          })}
-          classes={{
-            paper: clsx({
-              [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open,
-            }),
-          }}
-        >
-          <div className={classes.toolbar}>
-            {open ? (
-              <div>
-                <h1 style={{ color: "#fcaec7" }}>Oink</h1>
-                <IconButton onClick={handleDrawerClose}>
-                  <ChevronLeftIcon />
-                </IconButton>
-              </div>
-            ) : (
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                edge="start"
-                className={clsx(classes.menuButton, {
-                  [classes.hide]: open,
-                })}
-              >
-                <MenuIcon />
+          }),
+        }}
+      >
+        <div className={classes.toolbar}>
+          {open ? (
+            <div>
+              <h1 style={{ color: "#fcaec7" }}>Oink</h1>
+              <IconButton onClick={handleDrawerClose}>
+                <ChevronLeftIcon />
               </IconButton>
-            )}
-          </div>
-          <List>
-            <ListItem button key="home">
-              <ListItemIcon>
-                <HomeIcon />
-              </ListItemIcon>
-              <ListItemText primary="Home" />
-            </ListItem>
-            <ListItem button key="settings">
-              <ListItemIcon>
-                <SettingsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Settings" />
-            </ListItem>
-          </List>
-          <div className="circle1"></div>
-          <div className="circle2"></div>
-        </Drawer>
-        <main
-          className={clsx(classes.content, {
-            [classes.contentShift]: open,
-          })}
-        >
-          {children}
-        </main>
-      </ThemeProvider>
+            </div>
+          ) : (
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
+        </div>
+        <List>
+          <ListItem button key="home">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button key="settings">
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+        </List>
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+        <Logout drawerOpen={open} />
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        {children}
+      </main>
     </div>
   );
-});
+};
 
 AppPage.propTypes = {
   children: PropTypes.node.isRequired,
 };
 
-const mapStateToProps = createStructuredSelector({});
-
-export function mapDispatchToProps(dispatch) {
-  return {};
-}
-
-export default compose(memo)(AppPage);
+export default AppPage;
