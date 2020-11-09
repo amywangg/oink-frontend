7a2b5ae9a7b72367dbdcc3517d8e7cd9c4b7a34e
aneesaali <aneesaali@users.noreply.github.com>
2020-11-08 22:35:54 -0500

    shortened array code as per comments

--- src/containers/AppPage/Settings.js
+++ src/containers/AppPage/Settings.js
@@ -49,19 +49,7 @@ class Settings extends Component {
     }
 
     getDateMonth(monthNumber){
-      var month = new Array;
-      month[0] = "January";
-      month[1] = "February";
-      month[2] = "March";
-      month[3] = "April";
-      month[4] = "May";
-      month[5] = "June";
-      month[6] = "July";
-      month[7] = "August";
-      month[8] = "September";
-      month[9] = "October";
-      month[10] = "November";
-      month[11] = "December";
+      var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       return month[monthNumber];
     }
 
